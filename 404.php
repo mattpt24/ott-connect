531f83427bcf404a43d9b3fc6ed0dac02bc8a322
unknown <mattypt@hotmail.co.uk>
2022-10-10 14:33:28 +0200

    First commit of Back up OTT Connect

---
+++ 404.php
@@ -0,0 +1,12 @@
+
+<?php include "./includes/page-top.php";?>
+
+<section id="hero">
+    <div class="hero__content">
+        <h1 class="hero__title" style="font-weight: 900; font-size: 10rem; margin: 0; padding: 0;">404</h1>
+        <h1 class="hero__title" style="font-weight: 200; font-size: 2rem; margin: 0;">Oops! We couldn't find what you're looking for :(</h1>
+        <a href="#" class="btn btn--transparent">Back to Home Page</a>
+  </div>
+</section>
+
+<?php include "./includes/page-bottom.php";?>
